f5f8b2cb9e8e150cc37e14df9646e0fe6a7fbb76
Sajjarao Pavan Krishna <118989593+Pavan048@users.noreply.github.com>
2023-10-17 18:02:30 +0530

    Update contactController.js

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Contact = require("../models/contactModel");
 
-// @desc ....Get all contacts
+
 // @route .....GET /api/contacts
 // @access ......Private
 const getContacts = asyncHandler(async (req, res) => {
